e0aa7f0dd0606dbf6c99f45ad51c0fc383a82955
Anna Esteve <aesteve8@gmail.com>
2024-01-28 21:20:22 +0100

    homePage

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,37 @@
-import { Counter } from "@/components/Counter";
-import { Title } from "@/components/Title";
+import Button from "@/components/Button";
+import Header from "@/components/Header";
+import { Counter } from "@/components/test/Counter";
+import { Title } from "@/components/test/Title";
 import Image from "next/image";
-
 
 export default function Home() {
   return (
-    <main className="flex  flex-col items-center justify-between p-2">
-      <h1>Initial commit</h1>
-    </main>
+    <div className="container mx-auto">
+      <main className="w-full relative flex flex-col items-center justify-between py-3 px-6 bg-blue-500">
+        <Header />
+        <section className="section-first">
+          <p className="text-2xl">
+            <span className="text-mainRed font-semibold">ES EL SITIO.</span>{" "}
+            <span>ES LA PIZZA.</span>
+          </p>
+        </section>
+        <section className="flex flex-col justify-center items-center section-info">
+          <p>
+            En La Pizzeria se ha perfeccionado el arte de la masa hasta el
+            delirio. Fermentada con cariño durante 72 horas para conseguir la
+            textura perfecta.
+          </p>
+          <div className="flex gap-x-6 buttonWrapper">
+            <Button className="primary">Pide online</Button>
+            <Button className="primary">Nosotros</Button>
+          </div>
+        </section>
+        {/* <Image 
+        src={"/pizza_01.jpg"}
+        alt="pizza image"
+        fill={true}
+      /> */}
+      </main>
+    </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,15 @@
 const Button = ({ children, className = "", ...args }) => {
+  const isPrimary = className.includes("primary");
+  console.log("isPrimary", isPrimary);
+
   return (
     <button
-      className={`rounded-xl py-2 px-3 bg-blue-400 text-center text-white ${className}`}
+      className={`border rounded-[50px] py-3 px-6 ${
+        isPrimary
+          ? "text-mainRed hover:text-white bg-white hover:bg-mainRed  border-mainRed hover:border-white"
+          : "text-black bg-green-500"
+      }`}
+      style={{ boxShadow: "5px 3px 0px 0px #ac1823" }}
       {...args}
     >
       {children}
