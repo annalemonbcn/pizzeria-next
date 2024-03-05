87108366594bade296037317fa0c7b0fdbfc0afc
Anna Esteve <aesteve8@gmail.com>
2024-03-05 20:34:49 +0100

    admin role for admin route

---
+++ src/app/unhautorized/page.js
@@ -0,0 +1,22 @@
+"use client"
+import Button from "@/components/Button";
+import { useRouter } from "next/navigation";
+
+const Unhautorized = () => {
+  const router = useRouter();
+  return (
+    <>
+      <main className="w-full flex flex-col items-center mt-12">
+        <h1 className="text-4xl text-ellipsisxt-blue-600 my-4">
+          401 - Unhautorized
+        </h1>
+        <p className="mt-4">You don&apos;t have permission to acces this route</p>
+        <Button className="primary" onClick={() => router.replace("/")}>
+          Back home
+        </Button>
+      </main>
+    </>
+  );
+}
+
+export default Unhautorized
--- src/components/context/AuthContext.js
+++ src/components/context/AuthContext.js
@@ -3,7 +3,9 @@ import { toast } from "sonner";
 
 import { createContext, useContext, useState, useEffect } from "react";
 
-import { auth, provider } from "@/firebase/config";
+import { useRouter } from "next/navigation";
+
+import { auth, db, provider } from "@/firebase/config";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -11,11 +13,13 @@ import {
   onAuthStateChanged,
   signOut,
 } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
 
 const AuthContext = createContext();
 export const useAuthContext = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
+  const router = useRouter();
   const [user, setUser] = useState({
     logged: false,
     email: null,
@@ -47,13 +51,22 @@ export const AuthProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    onAuthStateChanged(auth, async (user) => {
       if (user) {
-        setUser({
-          logged: true,
-          email: user.email,
-          uid: user.uid,
-        });
+        const docRef = doc(db, "roles", user.uid);
+        const userDoc = await getDoc(docRef);
+
+        // console.log('userDoc', userDoc.data())
+        if (userDoc.data()?.rol === "admin") {
+          setUser({
+            logged: true,
+            email: user.email,
+            uid: user.uid,
+          });
+        } else {
+          router.push("/unhautorized");
+          logout();
+        }
       } else {
         setUser({
           logged: false,
